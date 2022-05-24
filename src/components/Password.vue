e8f9682e874c22452124da9f8ba2c0eb74fd43b0
vonroga <ryanroga@gmail.com>
2022-05-24 13:32:26 -0700

    Add "Copied!" Feedback
    App now says "Copied" once user has clicked and copied the text

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, computed } from "vue";
+import { defineProps, computed, ref } from "vue";
 import get from "../functions/get.js";
 
 const props = defineProps(["length", "special", "upper", "lower", "number"]);
@@ -8,15 +8,19 @@ const password = computed(() =>
   get(props.length, props.lower, props.upper, props.number, props.special)
 );
 
+const copied = ref(false);
+
 const copy = () => {
   navigator.clipboard.writeText(password.value);
-  //confirm(`Copied to clipboard: ${password.value}`);
+  copied.value = true;
 };
 </script>
 
 <template>
   <div class="p-4">
     <h3>Your password: (click to copy)</h3>
-    <div @click="copy" class="cursor-pointer font-bold">{{ password }}</div>
+    <div @click="copy" class="cursor-pointer font-bold">
+      {{ copied ? "Copied!" : password }}
+    </div>
   </div>
 </template>
